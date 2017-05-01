09d9e23d9eb7d71412c9ccf4a4afdd50ad422e57
Mahesh Mahimane <mahesh140494@gmail.com>
2017-05-01 14:35:02 +0000

    aries: build: Add AOSP-CAF 7.1 build support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_z3c.mk
+    $(LOCAL_DIR)/full_z3c.mk \
+    $(LOCAL_DIR)/aosp_z3c.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo lineage_z3c-eng
-add_lunch_combo lineage_z3c-userdebug
+add_lunch_combo aosp_z3c-eng
+add_lunch_combo aosp_z3c-userdebug
